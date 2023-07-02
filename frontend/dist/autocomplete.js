ea151f8b243a03b78c258eb454e5091183abf3b4
muratozcan8 <mrtzcn08@gmail.com>
2023-07-03 00:35:50 +0300

    Autocomplete is fixed
    Autocomplete connected to backend

--- frontend/dist/autocomplete.js
+++ frontend/dist/autocomplete.js
@@ -1,31 +1,19 @@
-let autocomplete;
+import { sendRequest } from "../send-request.js";
 
-// var script = document.createElement("script");
-// script.id = "autocomplete-script";
-// script.src = "https://maps.googleapis.com/maps/api/js?key=&callback=initMap&libraries=places&v=weekly";
-// document.head.appendChild(script);
 
-function searchAutocomplete(){
-  autocomplete = new google.maps.places.Autocomplete((document.getElementById('autocomplete')),
-  {
-    types:['geocode']
-  })
-
-  var searchBtn=document.getElementById('button-search-button');
-  searchBtn.onclick=function(){
-      findLocation();
-  }
-}
-
-function findLocation(){
-    var place = autocomplete.getPlace()
-    console.log(place.geometry.location.lat())
-    console.log(place.geometry.location.lng())
-}
+document.getElementById('autocomplete').addEventListener('input', onChangeLocation);
 
 function onChangeLocation(){
   var location = document.getElementById('autocomplete').value;
+  let URL = "http://localhost:8080/get/autocomplete?input=" + location;
+
+  sendRequest(URL, 'GET')
+        .then(data => {   
+            console.log(JSON.stringify(data.predictions))
+        })
+        .catch(err => {
+            console.error(err)
+        })
+
   console.log(location);
 }
-
-window.initMap = searchAutocomplete;
