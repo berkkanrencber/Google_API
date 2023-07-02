739542a6a2ee19f75c290cdcc5bb0e6e02a2af38
berkkanrencber <101739059+berkkanrencber@users.noreply.github.com>
2023-07-02 22:56:41 +0300

    Input control skeleton was finished

---
+++ frontend/dist/user-input-control.js
@@ -0,0 +1,19 @@
+
+
+
+var searchBtn=document.getElementById('button-search-button');
+  searchBtn.onclick=function(){
+    var typeValue=document.getElementById('input-type').value;
+
+
+var radiusValue=document.getElementById('radius-input-text').value;
+
+
+var ratingValue=document.getElementById('rating-input-text').value;
+    console.log(typeValue);
+    console.log(radiusValue);
+    console.log(ratingValue);
+
+  }
+
+
