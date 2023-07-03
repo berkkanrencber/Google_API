ef20a2acb3331788b633ff8d6481fa1c0b45a841
muratozcan8 <mrtzcn08@gmail.com>
2023-07-03 21:54:04 +0300

    list and selecting of location

--- frontend/dist/autocomplete.js
+++ frontend/dist/autocomplete.js
@@ -1,7 +1,9 @@
 import { sendRequest } from "../send-request.js";
 
-
-document.getElementById('autocomplete').addEventListener('input', onChangeLocation);
+let selectedLocationId;
+let resultBox = document.getElementById('result-box');
+let inputBox = document.getElementById('autocomplete');
+inputBox.addEventListener('input', onChangeLocation);
 
 function onChangeLocation(){
   var location = document.getElementById('autocomplete').value;
@@ -9,11 +11,42 @@ function onChangeLocation(){
 
   sendRequest(URL, 'GET')
         .then(data => {   
-            console.log(JSON.stringify(data.predictions))
+            console.log(JSON.stringify(data.predictions));
+            displayResults(data.predictions);
         })
         .catch(err => {
             console.error(err)
         })
 
   console.log(location);
+}
+
+function displayResults(result) {
+
+  const ul = document.createElement('ul');
+
+    for (let i = 0; i < result.length; i++) {
+        const li = document.createElement('li');
+        li.className = "list-none rounded-sm py-2 px-2 bg-white cursor-pointer hover:bg-slate-300";
+        li.textContent = result[i].description;
+        li.onclick = () => {
+          selectInput(result[i].description);
+          setSelectedLocationId(result[i].place_id);
+        }
+        ul.appendChild(li);
+    }
+
+    resultBox.innerHTML = '';
+    resultBox.appendChild(ul);
+}
+
+function selectInput(list) {
+  console.log(list);
+  inputBox.value = list;
+  resultBox.innerHTML = '';
+}
+
+function setSelectedLocationId(id){
+  console.log(id);
+  selectedLocationId = id;
 }
