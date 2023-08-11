2e88f80492bc5f2e8c63763f860abfa630f6f5ff
alikosar <ali.kosarr@hotmail.com>
2023-08-11 19:00:32 +0300

    Sorting according to label was completed

--- frontend/dist/place-list.js
+++ frontend/dist/place-list.js
@@ -1,56 +1,86 @@
 import { sendRequest } from "../send-request.js";
 import { place_types } from "./place-types.js";
-import { getLocationId,getLocationLatLng } from "./autocomplete.js";
-import { getMarkedPlaceId,placeMarkerFromMap,deleteMarkers,setCenterOfMap,setCenterOfMapClick } from "./geocoding.js";
+import { getLocationId, getLocationLatLng } from "./autocomplete.js";
+import { getMarkedPlaceId, placeMarkerFromMap, deleteMarkers, setCenterOfMap, setCenterOfMapClick } from "./geocoding.js";
 
 let places;
-function clickRequest(){
-  if(document.getElementById('autocomplete').value!=""){
+let current_places;
+function clickRequest() {
+    if (document.getElementById('autocomplete').value != "") {
 
-    var typeValue=document.getElementById('input-type');
-    var radiusValue=document.getElementById('radius-input-text').value;
-    var ratingValue=document.getElementById('rating-input-text').value;
-    var place_id = getLocationId() ? getLocationId() : getMarkedPlaceId();
-    var totalValue=document.getElementById('total-rating-input-text').value;
-    let URL = `http://localhost:8080/get/nearby_search?place_id=${place_id}&radius=${radiusValue}&type=${place_types[typeValue.selectedIndex]}&rating=${ratingValue}&user_ratings_total=${totalValue}`
+        var typeValue = document.getElementById('input-type');
+        var radiusValue = document.getElementById('radius-input-text').value;
+        var ratingValue = document.getElementById('rating-input-text').value;
+        var place_id = getLocationId() ? getLocationId() : getMarkedPlaceId();
+        var totalValue = document.getElementById('total-rating-input-text').value;
+        let URL = `http://localhost:8080/get/nearby_search?place_id=${place_id}&radius=${radiusValue}&type=${place_types[typeValue.selectedIndex]}&rating=${ratingValue}&user_ratings_total=${totalValue}`
 
-    sendRequest(URL, 'GET')
-        .then(data => {   
-            places = data;
-            console.log(getLocationLatLng())
-            if(Object.keys(getLocationLatLng()).length>0){
-                setCenterOfMap(getLocationLatLng(),(16-(radiusValue/500)));
-            }else{
-                setCenterOfMapClick(16-(radiusValue/500));
-            }
-            deleteMarkers();
-            createMarker(places.results);
-            createPageButtons(places.results,8);
-        })
-        .catch(err => {
-            console.error(err)
-        })
+        sendRequest(URL, 'GET')
+            .then(data => {
+                places = data;
+                console.log(getLocationLatLng())
+                if (Object.keys(getLocationLatLng()).length > 0) {
+                    setCenterOfMap(getLocationLatLng(), (16 - (radiusValue / 500)));
+                } else {
+                    setCenterOfMapClick(16 - (radiusValue / 500));
+                }
+                deleteMarkers();
+                createMarker(places.results);
+                places.results = sortElement(places.results, 'name');
+                document.getElementById('lbl-name').className="btn-primary text-white";
 
-    loadingAnimation();
-  }
+
+                createPageButtons(places.results, 8);
+            })
+            .catch(err => {
+                console.error(err)
+            })
+
+        loadingAnimation();
+        document.getElementById('lbl-name').addEventListener('click', function() {
+            places.results = sortElement(places.results.slice(), 'name'); 
+            createPageButtons(places.results, 8);
+            document.getElementById('lbl-name').className="btn-primary text-white";
+            document.getElementById('lbl-rating').className="btn-primary"
+            document.getElementById('lbl-vote').className="btn-primary"
+        });
+        document.getElementById('lbl-rating').addEventListener('click', function() {
+            places.results = sortElement(places.results.slice(), 'rating'); 
+            createPageButtons(places.results, 8);
+            document.getElementById('lbl-name').className="btn-primary";
+            document.getElementById('lbl-rating').className="btn-primary text-white"
+            document.getElementById('lbl-vote').className="btn-primary"
+            
+        });
+        document.getElementById('lbl-vote').addEventListener('click', function() {
+            places.results = sortElement(places.results.slice(), 'vote'); 
+            createPageButtons(places.results, 8);
+            document.getElementById('lbl-name').className="btn-primary";
+            document.getElementById('lbl-rating').className="btn-primary"
+            document.getElementById('lbl-vote').className="btn-primary text-white"
+        });
+
+    }
+
 }
 
 document.getElementById('button-search-button').addEventListener('click', clickRequest);
 document.getElementById('export-btn').style.visibility = "hidden";
 
-function createMarker(places_array){
-    for(let place of places_array){
-        placeMarkerFromMap(place.name,place.location);
+
+function createMarker(places_array) {
+    for (let place of places_array) {
+        placeMarkerFromMap(place.name, place.location);
     }
 }
 
-function loadingAnimation(){
-    document.querySelector("#data-output").innerHTML="";
+function loadingAnimation() {
+    document.querySelector("#data-output").innerHTML = "";
     let loading = document.querySelector("#paging-buttons");
-    loading.innerHTML="";
+    loading.innerHTML = "";
     let i;
-    for(i=0; i<8; i++)
-    loading.innerHTML += `<div class="border-b-2 border-gray-600 shadow m-4 p-4 max-w-xxl w-full mx-auto">
+    for (i = 0; i < 8; i++)
+        loading.innerHTML += `<div class="border-b-2 border-gray-600 shadow m-4 p-4 max-w-xxl w-full mx-auto">
   <div class="animate-pulse  flex space-x-4">
     <div class=" bg-slate-700 h-7 w-7"></div>
     <div class="flex-1 space-y-6 py-1">
@@ -66,22 +96,23 @@ function loadingAnimation(){
 </div>`
 }
 
-function getPlacesWithPage(places_array,page,limit){
-    const startIndex= (page-1)*limit;
-    const endIndex= page*limit;
+function getPlacesWithPage(places_array, page, limit) {
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
 
     let results = [];
-    results= places_array.slice(startIndex,endIndex);
+    results = places_array.slice(startIndex, endIndex);
+
 
     //to list all wished places
     let placeholder = document.querySelector("#data-output");
     let out = "";
-    for(let place of results){
-            out += `
+    for (let place of results) {
+        out += `
                 <tr>
                     <th>
                         <label>
-                            <input type="checkbox" class="checkbox" id="c${place.place_id}" ${selected_places.includes(`c${place.place_id}`)?'checked':''}/>
+                            <input type="checkbox" class="checkbox" id="c${place.place_id}" ${selected_places.includes(`c${place.place_id}`) ? 'checked' : ''}/>
                         </label>
                     </th>
                     <td class="text-center">
@@ -100,103 +131,135 @@ function getPlacesWithPage(places_array,page,limit){
                     </th>
                 </tr>
             `;
-            placeholder.innerHTML = out;
+        placeholder.innerHTML = out;
     }
-    for(let i = 0; i < results.length; i++){
+    for (let i = 0; i < results.length; i++) {
         document.getElementsByClassName('drawer-button')[i].addEventListener('click', fetchPlaceDetails);
         document.getElementsByClassName('drawer-button')[i].param = document.getElementsByClassName('drawer-button')[i].id;
         document.getElementsByClassName('checkbox')[i].addEventListener('change', checkSelectedPlaces);
     }
+
 }
 
-let selected_places=[];
-function checkSelectedPlaces(){
-    if(this.checked){
+let selected_places = [];
+function checkSelectedPlaces() {
+    if (this.checked) {
         selected_places.push(this.id)
         document.getElementById('export-btn').style.visibility = "visible";
     }
-    
-    else{
+
+    else {
         const index = selected_places.indexOf(this.id);
         if (index > -1) // only splice array when item is found
-         selected_places.splice(index, 1); // 2nd parameter means remove one item only
-        if(selected_places.length==0)
-         document.getElementById('export-btn').style.visibility = "hidden";
+            selected_places.splice(index, 1); // 2nd parameter means remove one item only
+        if (selected_places.length == 0)
+            document.getElementById('export-btn').style.visibility = "hidden";
     }
 }
-document.getElementById('export-btn').addEventListener('click',click_export_button);
-async function click_export_button(){
+document.getElementById('export-btn').addEventListener('click', click_export_button);
+async function click_export_button() {
     var base_url = "http://localhost:8080/get/place_detail?place_id="
     var csv = "Mekan adı, Puan, Değerlendirme Sayısı, Telefon, Adres\n"
-    for(var place_id of selected_places){
-        var url = base_url +place_id.substring(1);
+    for (var place_id of selected_places) {
+        var url = base_url + place_id.substring(1);
         await sendRequest(url, 'GET')
-        .then(data => {
-            var address = data.formatted_address.replaceAll(",","");
-            csv += `${data.name}, ${data.rating}, ${data.user_ratings_total}, ${data.international_phone_number}, ${address}\n`;
-        })
-        .catch(err => {
-            console.error(`Error: ${err}`);
-        })
+            .then(data => {
+                var address = data.formatted_address.replaceAll(",", "");
+                csv += `${data.name}, ${data.rating}, ${data.user_ratings_total}, ${data.international_phone_number}, ${address}\n`;
+            })
+            .catch(err => {
+                console.error(`Error: ${err}`);
+            })
     }
     downloadCSV(csv);
 
 }
-function downloadCSV(csv){
+function downloadCSV(csv) {
     var hiddenElement = document.createElement('a');
     hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
     hiddenElement.target = '_blank';
-    
+
     //provide the name for the CSV file to be downloaded
     hiddenElement.download = 'Seçilen Mekanlar.csv';
     hiddenElement.click();
 }
 
 
-function fetchPlaceDetails(place_id){
-    
-    place_name.innerHTML="";
-    place_rating.innerHTML="";
-    place_address.innerHTML ="";
-    place_phone.innerHTML ="";
-    place_user_total_rating.innerHTML ="";
+function fetchPlaceDetails(place_id) {
+
+    place_name.innerHTML = "";
+    place_rating.innerHTML = "";
+    place_address.innerHTML = "";
+    place_phone.innerHTML = "";
+    place_user_total_rating.innerHTML = "";
     place_url.innerHTML = "";
-    place_url.href ="";
-    place_review.innerHTML="";
-    
+    place_url.href = "";
+    place_review.innerHTML = "";
+
     let URL = `http://localhost:8080/get/place_detail?place_id=${place_id.currentTarget.param}`
     console.log("girdi")
     sendRequest(URL, 'GET')
-        .then(data => {   
+        .then(data => {
             fetchDetails(data);
         })
         .catch(err => {
             console.error(err)
         })
 
-}  
+}
 
-function createPageButtons(places_array,limit){
+function sortElement(places_array, label) {
+    console.log(label +" için sorta girdi")
+    if (label == 'name') {
+        places_array.sort((a, b) => { //
+            const nameA = a.name.toLowerCase(); // Convert names to uppercase for case-insensitive sorting
+            const nameB = b.name.toLowerCase();
+
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+    else if(label=='vote'){
+        places_array.sort((a, b) => { //
+            return b.user_ratings_total - a.user_ratings_total
+        });
+    }
+    else {
+        places_array.sort((a, b) => { //
+            return b.rating - a.rating
+        });
+    }
+    return places_array;
+    
+}
+
+function createPageButtons(places_array, limit) {
     const lastPage = Math.ceil(places_array.length / limit);
 
-//to show exact paging button numbers
+
+    //to show exact paging button numbers
     let placeholder_btn = document.querySelector("#paging-buttons");
     let out_btn = "";
     let i;
-    for(i=1; i<=lastPage; i++){
-            out_btn += `
+    for (i = 1; i <= lastPage; i++) {
+        out_btn += `
             <button class="join-item btn" id="btn${i}">${i}</button>
             `;
     }
     placeholder_btn.innerHTML = out_btn;
 
-    getPlacesWithPage(places_array,1,limit);
+    getPlacesWithPage(places_array, 1, limit);
 
     let buttons = document.querySelectorAll('.join-item');
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             let page = parseInt(button.innerText);
-            getPlacesWithPage(places.results,page, 8);
+            getPlacesWithPage(places.results, page, 8);
         })
     })
 }
@@ -211,10 +274,10 @@ const place_weekday_text = document.getElementById('place-weekday-text');
 const place_type = document.getElementById('place-types');
 const place_review = document.getElementById('place-reviews');
 
-function fetchDetails(place_details_array){
+function fetchDetails(place_details_array) {
 
     let reviews = [];
-    reviews = place_details_array.reviews; 
+    reviews = place_details_array.reviews;
 
     place_name.innerHTML = place_details_array.name;
     place_rating.innerHTML = place_details_array.rating;
@@ -227,23 +290,23 @@ function fetchDetails(place_details_array){
 
 
     place_weekday_text.innerHTML = "";
-    for(let i = 0; i < place_details_array.weekday_text.length; i++){
+    for (let i = 0; i < place_details_array.weekday_text.length; i++) {
         var li = document.createElement("li");
         li.appendChild(document.createTextNode(place_details_array.weekday_text[i]));
         place_weekday_text.appendChild(li);
     }
 
     place_type.innerHTML = "";
-    for(let i = 0; i < place_details_array.types.length; i++){
+    for (let i = 0; i < place_details_array.types.length; i++) {
         var li = document.createElement("li");
         li.appendChild(document.createTextNode(place_details_array.types[i]));
         place_type.appendChild(li);
     }
 
-    let out="";
-    let index=0;
-    for(let review of reviews){
-        out+=`
+    let out = "";
+    let index = 0;
+    for (let review of reviews) {
+        out += `
         <div class="chat chat-start">
         <div class="chat-image avatar">
           <div class="w-10 rounded-full">
@@ -266,12 +329,12 @@ function fetchDetails(place_details_array){
         </div>
         </div>
         `
-        place_review.innerHTML=out;
+        place_review.innerHTML = out;
         index++;
     }
 
-    for(let i=0; i<reviews.length; i++){
-        document.getElementById(`star-${reviews[i].rating}-${i}`).checked=true;
+    for (let i = 0; i < reviews.length; i++) {
+        document.getElementById(`star-${reviews[i].rating}-${i}`).checked = true;
     }
-    
+
 }
